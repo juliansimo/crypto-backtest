f3a692e51f9d67ea78d1a6b389c02e189a1ef623
Julian <julian.simo@gmail.com>
2025-08-02 07:23:35 -0300

    needs some improvement, but works

--- backtest.py
+++ backtest.py
@@ -1,43 +1,52 @@
 import pandas as pd
 
 # parameters
+list_of_assets = ["01_btc", "02_eth", "03_sol", "04_xrp"]
 leverage_list = [0.0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
 starting_year = 2020
-simulated_pnl = []
+
+# output dataframe
+df_ans = pd.DataFrame(leverage_list, columns=["leverage"])
 
 # read data
-df = pd.read_csv("01_btc_historical_data.csv")
+for asset in list_of_assets:
 
-# convert 'data' column to datetime and set it as index
-df["data"] = pd.to_datetime(df["data"], format="%Y-%m-%d")
-df.set_index("data", inplace=True)
+    # read the CSV file for each asset
+    print(f"reading data for {asset}...")
+    df = pd.read_csv(f"{asset}_historical_data.csv")
 
-# drop NaN indexed lines and ensure 'close' is float
-df = df[~df.index.isna()] 
-df["close"] = df["close"].astype(float)
+    # convert 'data' column to datetime and set it as index
+    df["data"] = pd.to_datetime(df["data"], format="%Y-%m-%d")
+    df.set_index("data", inplace=True)
 
-# filter data for the specified year
-df = df[df.index.year >= starting_year]
+    # drop NaN indexed lines and ensure 'close' is float
+    df = df[~df.index.isna()] 
+    df["close"] = df["close"].astype(float)
 
-# set initial backtest conditions
-initial_price = df["close"].iloc[0]
-cripto_return = 0.0
-future_return = 0.0
-total_return = 0.0
-pnl = 0.0
-liquidated = False
+    # filter data for the specified year
+    df = df[df.index.year >= starting_year]
 
-# backtest loop
-for leverage in leverage_list:
-    for index, row in df.iterrows():
-        if not liquidated:
-            cripto_return = row["close"] / initial_price - 1
-            future_return = leverage * cripto_return
-            total_return = cripto_return + future_return
-            if total_return < -1:
-                liquidated = True
-                total_return = -1.0
-            pnl = 1 + total_return
-    print(f"{index:%Y-%m-%d}: leverage = {leverage:.2f} | PnL = {pnl:.2f}")
+    # backtest loop
+    simulated_pnl = []
+    for leverage in leverage_list:
+        initial_price = df["close"].iloc[0]
+        cripto_return = 0.0
+        future_return = 0.0
+        total_return = 0.0
+        pnl = 0.0
+        liquidated = False
+        for index, row in df.iterrows():
+            if not liquidated:
+                cripto_return = row["close"] / initial_price - 1
+                future_return = leverage * cripto_return
+                total_return = cripto_return + future_return
+                if total_return < -1:
+                    liquidated = True
+                    total_return = -1.0
+                pnl = 1 + total_return
+        simulated_pnl.append(float(pnl))
+        # print(f"{index:%Y-%m-%d}: leverage = {leverage:.2f} | PnL = {pnl:.2f}")
+    df_ans[asset] = simulated_pnl
 
-#print(f"{index:%Y-%m-%d}: crypto = {cripto_return:.2f} | future = {future_return:.2f} | total = {total_return:.2f}")
+df_ans.set_index("leverage", inplace=True)
+print(df_ans)
