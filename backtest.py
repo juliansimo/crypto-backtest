955033846e7f951db7d9f4e3b764fce4ed6298f7
Julian <julian.simo@gmail.com>
2025-08-02 06:40:39 -0300

    first functional version

--- backtest.py
+++ backtest.py
@@ -1,30 +1,41 @@
 import pandas as pd
 
 # parameters
-leverage = 2.0
-starting_year = 2018
+leverage = 0.0
+starting_year = 2020
 
-# read data and make necessary adjustments
+# read data
 df = pd.read_csv("01_btc_historical_data.csv")
+
+# convert 'data' column to datetime and set it as index
 df["data"] = pd.to_datetime(df["data"], format="%Y-%m-%d")
 df.set_index("data", inplace=True)
 
-# drop NaN indexed lines
-df = df[~df.index.isna()]
+# drop NaN indexed lines and ensure 'close' is float
+df = df[~df.index.isna()] 
+df["close"] = df["close"].astype(float)
 
 # filter data for the specified year
 df = df[df.index.year >= starting_year]
 
-# makes sure prices are float (not str)
-df["close"] = df["close"].astype(float)
-
 # set initial backtest conditions
 initial_price = df["close"].iloc[0]
-cripto_return = 1.0
-future_return = 1.0
+cripto_return = 0.0
+future_return = 0.0
+total_return = 0.0
+pnl = 0.0
+liquidated = False
 
+# backtest loop
 for index, row in df.iterrows():
-    cripto_return = row["close"] / initial_price - 1
-    future_return = leverage * cripto_return
-    total_return = cripto_return + future_return
-    print(f"return for {index} = {cripto_return} | future return = {future_return} | total return = {total_return}")
+    if not liquidated:
+        cripto_return = row["close"] / initial_price - 1
+        future_return = leverage * cripto_return
+        total_return = cripto_return + future_return
+        if total_return < -1:
+            liquidated = True
+            total_return = -1.0
+        pnl = 1 + total_return
+        print(f"{index:%Y-%m-%d}: PnL = {pnl:.2f}")
+
+#print(f"{index:%Y-%m-%d}: crypto = {cripto_return:.2f} | future = {future_return:.2f} | total = {total_return:.2f}")
