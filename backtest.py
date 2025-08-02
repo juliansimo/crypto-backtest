70f03930f92ff0488a092c7c3fd349cff561d20b
Julian <julian.simo@gmail.com>
2025-08-02 06:47:57 -0300

    working with multiple leverage levels

--- backtest.py
+++ backtest.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 # parameters
-leverage = 0.0
+leverage_list = [0.0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
 starting_year = 2020
+simulated_pnl = []
 
 # read data
 df = pd.read_csv("01_btc_historical_data.csv")
@@ -27,15 +28,16 @@ pnl = 0.0
 liquidated = False
 
 # backtest loop
-for index, row in df.iterrows():
-    if not liquidated:
-        cripto_return = row["close"] / initial_price - 1
-        future_return = leverage * cripto_return
-        total_return = cripto_return + future_return
-        if total_return < -1:
-            liquidated = True
-            total_return = -1.0
-        pnl = 1 + total_return
-        print(f"{index:%Y-%m-%d}: PnL = {pnl:.2f}")
+for leverage in leverage_list:
+    for index, row in df.iterrows():
+        if not liquidated:
+            cripto_return = row["close"] / initial_price - 1
+            future_return = leverage * cripto_return
+            total_return = cripto_return + future_return
+            if total_return < -1:
+                liquidated = True
+                total_return = -1.0
+            pnl = 1 + total_return
+    print(f"{index:%Y-%m-%d}: leverage = {leverage:.2f} | PnL = {pnl:.2f}")
 
 #print(f"{index:%Y-%m-%d}: crypto = {cripto_return:.2f} | future = {future_return:.2f} | total = {total_return:.2f}")
